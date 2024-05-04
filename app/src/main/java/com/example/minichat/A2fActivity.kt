ffab1d1461823482220fd19df572a10ef7e0453a
0JD0 <eusojdivad0@gmail.com>
2024-05-04 14:13:49 -0600

    First day, Login, A2f and Register: Id assignment and button mapping

--- app/src/main/java/com/example/minichat/A2fActivity.kt
+++ app/src/main/java/com/example/minichat/A2fActivity.kt
@@ -1,11 +1,26 @@
 package com.example.minichat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class A2fActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_a2f)
+
+        val btnNext : Button = findViewById(R.id.btnNextA2f)
+        val btnBack : Button = findViewById(R.id.btnBackA2f)
+
+        btnNext.setOnClickListener() {
+            val intent = Intent(this, ChatsActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnBack.setOnClickListener() {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
